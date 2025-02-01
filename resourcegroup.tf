9804905bbfd0d36b4a254c73b70f7d3ba3a57541
ajirijames <ajirijames1@gmail.com>
2025-01-31 19:58:13 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,16 +1,16 @@
 /*resource "azurerm_resource_group" "mcit420zz5um" {
   name     = "mcit_resource_group_ajiri"
   location = "centralus"
-} */
-resource "azurerm_resource_group" "mcit420zz5um2" {
+}*/
+/*resource "azurerm_resource_group" "mcit420zz5um2" {
   name     = "mcit_resource_group_james"
   location = "southcentralus"
-}
-resource "azurerm_resource_group" "mcit420zz5um3" {
+}*/
+/*resource "azurerm_resource_group" "mcit420zz5um3" {
   name     = "mcit_resource_group_ajiri_3"
   location = "eastus"
-}
-resource "azurerm_resource_group" "mcit420zz5um4" {
+}*/
+/*resource "azurerm_resource_group" "mcit420zz5um4" {
   name     = "mcit_resource_group_james_4"
   location = "canadacentral"
-}
+}*/
