9cccc3ac65cda0d3a4f10c568705167968a81b21
ajirijames <ajirijames1@gmail.com>
2025-02-07 20:10:17 -0500

    Create things.tf

---
+++ things.tf
@@ -0,0 +1,28 @@
+locals {
+  # List of 5 restaurants in Montreal
+  montreal_restaurants = [
+    "Zibo",
+    "Bouillon Bilk",
+    "Modavie",
+    "Brasserie 701",
+    "Au Pied de Cochon"
+  ]
+
+  # Top 5 places you want to visit
+  places_to_visit = [
+    "Disneyland",
+    "Grand Canyon",
+    "Paris",
+    "Dubai",
+    "Santorini"
+  ]
+
+  # Top 5 favorite countries you want to visit
+  favorite_countries = [
+    "Jamaica",
+    "Brazil",
+    "New Zealand",
+    "France",
+    "Italy"
+  ]
+}
