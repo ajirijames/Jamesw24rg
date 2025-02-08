b003324fc8559d8dd838eda8d316abaade0cf6ff
ajirijames <ajirijames1@gmail.com>
2025-02-07 20:10:52 -0500

    Create outputvariable.tf

---
+++ outputvariable.tf
@@ -0,0 +1,11 @@
+output "montreal_restaurants" {
+  value = local.montreal_restaurants
+}
+
+output "places_to_visit" {
+  value = local.places_to_visit
+}
+
+output "favorite_countries" {
+  value = local.favorite_countries
+}
