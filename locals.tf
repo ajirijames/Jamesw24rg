ce0f07d12c7540d4d9b20f16538578d916e0a86d
ajirijames <ajirijames1@gmail.com>
2025-02-19 19:57:19 -0500

    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,6 @@
+locals{
+  storage_account_name  = "ajstorageaccount"
+  resource_group_name   = "ajresourcegroup"
+  location              = "canadacentral"
+  account_tier          = "Standard"
+}
