151777240e1b60b5820dee71cf5c76c532ef700d
ajirijames <ajirijames1@gmail.com>
2025-02-19 20:01:55 -0500

    Create moduleoutput.tf

---
+++ moduleoutput.tf
@@ -0,0 +1,16 @@
+output "storage_account_name" {
+ description   = "ajstorageaccount"
+ value         = local.storage_account_name
+}
+output "resource_group_name" {
+ description   = "ajresourcegroup"
+ value         = local.resource_group_name
+}
+output "location" {
+ description   = "canadacentral"
+ value         = local.location
+}
+output "account_tier" {
+ description   = "standard"
+ value         = local.account_tier
+}
